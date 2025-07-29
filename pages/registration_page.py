aa62bd04d65cc98ce746457f8a1436d3e14237a0
Sergei Kozlov <sergeo.kozlov@gmail.com>
2025-07-29 10:36:39 +0200

    Practice working with the Page Factory

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,15 +1,16 @@
 from components.authentication.registration_form_component import RegistrationFormComponent
 from pages.base_page import BasePage
-from playwright.sync_api import Page, expect
-
+from playwright.sync_api import Page
+from elements.button import Button
+from elements.link import Link
 
 class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
         self.registration_form = RegistrationFormComponent(page)
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
-        self.login_link = page.get_by_test_id('registration-page-login-link')
+        self.registration_button = Button(page, 'registration-page-registration-button', 'REGISTRATION')
+        self.login_link = Link(page, 'registration-page-login-link', 'Login')
 
     def click_registration_button(self):
         self.registration_button.click()
--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -15,4 +15,4 @@ def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str
     login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
     login_page.login_form.fill(email=email, password=password)
     login_page.click_login_button()
-    login_page.check_visible_wrong_email_password_alert()
+    login_page.check_visible_wrong_email_or_password_alert()

0b15f65c6b91f7ebee90f3ad0531efa3b1dc7fc7
Sergei Kozlov <sergeo.kozlov@gmail.com>
2025-07-30 16:54:19 +0200

    Building up the test base and refactoring

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -3,6 +3,8 @@ import pytest
 from playwright.sync_api import Page, Playwright
 from typing import Generator
 
+from pages.authentication.registration_page import RegistrationPage
+
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Generator[Page, None, None]:
@@ -18,21 +20,17 @@ def initialize_browser_state(playwright: Playwright):
     context = browser.new_context()
     page = context.new_page()
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
-    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
-
-    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('username')
-
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
-
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+    registration_page = RegistrationPage(page=page)
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.registration_form.fill(
+        email='user.name@gmail.com',
+        username='username',
+        password='password'
+    )
+    registration_page.click_registration_button()
 
     context.storage_state(path='browser-state.json')
+    browser.close()
 
 @pytest.fixture(scope='function')
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Generator[Page, None, None]:

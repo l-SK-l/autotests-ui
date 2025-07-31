c768236fb7ffe3443ba07e538fb891c9ab2cefd1
Sergei Kozlov <sergeo.kozlov@gmail.com>
2025-07-31 09:40:36 +0200

    Integrating Allure into autotests

--- tests/authentication/test_registration.py
+++ tests/authentication/test_registration.py
@@ -1,42 +1,34 @@
+import allure
 import pytest
+from allure_commons.types import Severity
 
-from pages.courses.courses_list_page import CoursesListPage
-from pages.courses.create_course_page import CreateCoursePage
+from pages.authentication.registration_page import RegistrationPage
+from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 
 
-@pytest.mark.courses
 @pytest.mark.regression
-class TestCourses:
-    def test_empty_courses_list(self, courses_list_page: CoursesListPage):
-        courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-        courses_list_page.navbar.check_visible('username')
-        courses_list_page.sidebar.check_visible()
-        courses_list_page.toolbar_view.check_visible()
-        courses_list_page.check_visible_empty_view()
-
-    def test_create_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
-        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
-        create_course_page.create_course_toolbar_view.check_visible()
-        create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
-        create_course_page.create_course_form.check_visible(
-            title="", estimated_time="", description="", max_score="0", min_score="0"
+@pytest.mark.registration
+@allure.tag(AllureTag.REGRESSION, AllureTag.REGISTRATION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStory.REGISTRATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.REGISTRATION)
+class TestRegistration:
+    @allure.title("Registration with correct email, username and password")
+    @allure.severity(Severity.CRITICAL)
+    def test_successful_registration(self, dashboard_page: DashboardPage, registration_page: RegistrationPage):
+        registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+        registration_page.registration_form.fill(
+            email="user.name@gmail.com",
+            username="username",
+            password="password"
         )
+        registration_page.click_registration_button()
 
-        create_course_page.create_course_exercises_toolbar_view.check_visible()
-        create_course_page.check_visible_exercises_empty_view()
-
-        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
-        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
-        create_course_page.create_course_form.fill(
-            title="Playwright",
-            estimated_time="2 weeks",
-            description="Playwright",
-            max_score="100",
-            min_score="10"
-        )
-        create_course_page.create_course_toolbar_view.click_create_course_button()
-
-        courses_list_page.toolbar_view.check_visible()
-        courses_list_page.course_view.check_visible(
-            index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
-        )
+        dashboard_page.dashboard_toolbar_view.check_visible()

bef351f83f058b526673f46654e92d789750becf
Sergei Kozlov <sergeo.kozlov@gmail.com>
2025-08-01 13:45:56 +0200

    Adding Allure steps to autotests

--- pages/courses/courses_list_page.py
+++ pages/courses/courses_list_page.py
@@ -1,4 +1,5 @@
-from playwright.sync_api import Page, expect
+import allure
+from playwright.sync_api import Page
 
 from components.courses.course_view_component import CourseViewComponent
 from components.courses.courses_list_toolbar_view_component import CoursesListToolbarViewComponent
@@ -18,6 +19,7 @@ class CoursesListPage(BasePage):
         self.sidebar = SidebarComponent(page)
         self.empty_view = EmptyViewComponent(page, 'courses-list')
 
+    @allure.step('Check visible empty view "{title}"')
     def check_visible_empty_view(self):
         self.empty_view.check_visible(
             title='There is no results',
